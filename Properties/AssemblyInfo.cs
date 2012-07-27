d6039506a64a0eaf64d1027b8122886ddad0f993
Bowofola <bowofola@gmail.com>
2012-07-27 00:35:58 -0400

    Version1 Release

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WindowsPhoneSpeechTools")]
+[assembly: AssemblyDescription("Text-to-speech and language translation tools for Windows Phone using Microsoft Translate API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bowofola")]
+[assembly: AssemblyProduct("WindowsPhoneSpeechTools")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fca83d64-7f06-4e91-aa6a-21b658067dae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
---
+++ SpeechServices.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Net;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using WPhoneMSTAccessToken;
+using WPhoneSpeechTools.ServiceMSTranslator;
+
+namespace WPhoneSpeechTools
+{
+    public class SpeechService
+    {
+        #region private members
+
+        private Action _action;
+        private string _quality = "";
+        private LanguageServiceClient _serviceClient;
+        private string _language = "";
+        //private string[] _languages;
+        
+        //private string _appId = "";
+        private string _clientID = "";
+        private string _clientSecret = "";
+
+        private AdmAuthentication admAuth;
+        private AdmAccessToken _token;
+
+        private string _textToSpeak;
+
+        #endregion
+
+        #region properties
+
+        public enum Action
+        {
+            Speak
+        }
+
+        public enum SpeechQuality
+        {
+            Max,
+            Min
+        }
+
+
+        public string Language
+        {
+            get { return _language; }
+            set { _language = value; }
+        }
+
+
+        public SpeechQuality Quality
+        {
+            set 
+            {
+                if (value == SpeechQuality.Max)
+                    _quality = "MaxQuality";
+                if (value == SpeechQuality.Min)
+                    _quality = "MinSize";
+            }
+        }
+
+        #endregion
+
+        public SpeechService(string clientID, string clientSecret)
+        {
+            //_appId = appId;
+            _clientID = clientID;
+            _clientSecret = clientSecret;
+            _serviceClient = new LanguageServiceClient();
+            HookupEvents();
+            admAuth = new AdmAuthentication(_clientID, _clientSecret);
+            admAuth.returnToken += RetrieveToken;
+        }
+
+        
+
+        #region Event Handlers
+
+        private void translator_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            var client = new WebClient();
+            lock (client)
+            {
+                client.OpenReadCompleted += ((s, args) =>
+                    {
+                        FrameworkDispatcher.Update();
+                        SoundEffect se = SoundEffect.FromStream(args.Result);
+                        lock (se)
+                        {
+                            se.Play();
+                        }
+                    }
+                );
+                client.OpenReadAsync(new Uri(e.Result));
+            }
+        }
+
+        #endregion
+
+        #region Public
+
+        public void SpeakText(string text)
+        {
+            if(String.IsNullOrWhiteSpace(_quality))
+                _quality = "MinSize";
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                _textToSpeak = text;
+                admAuth.GetToken();
+                _action = Action.Speak;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void HookupEvents()
+        {
+            _serviceClient.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(translator_SpeakCompleted);
+
+        }
+
+        private void RetrieveToken(AdmAccessToken token)
+        {
+            _token = token;
+            PerformAction();
+        }
+
+        private void PerformAction()
+        {
+            if (_action == Action.Speak)
+            {
+                string headerValue = "Bearer " + _token.access_token;
+                _serviceClient.SpeakAsync(headerValue, _textToSpeak, _language, "audio/wav", _quality);
+            }
+        }
+
+        #endregion
+    }
+}
